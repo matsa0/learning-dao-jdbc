e8d674cdfa89adccc97047ef35ffe249d8ab1c0a
Matheus Azevedo <matheus.azevedo@aluno.ufop.edu.br>
2024-01-08 16:53:29 -0300

    Implementando findById e Update

--- src/application/App2.java
+++ src/application/App2.java
@@ -7,10 +7,20 @@ import model_entities.Department;
 public class App2 {
     public static void main(String[] args) throws Exception {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-        
-        System.out.println("=== TEST 1: department insert ===");
         Department dep1 = new Department("Security");
+        /*
+        System.out.println("=== TEST 1: department insert ===");
         departmentDao.insert(dep1);
+        System.out.println(dep1);
+        */
+        System.out.println("=== TEST 2: department findById ===");
+        dep1 = departmentDao.findById(3);
+        System.out.println(dep1);
+
+        System.out.println("=== TEST 2: department update ===");
+        dep1 = departmentDao.findById(12);
+        dep1.setName("Games");
+        departmentDao.update(dep1);
         System.out.println(dep1);
     }
 }
--- src/model_dao_impl/DepartmentDaoJDBC.java
+++ src/model_dao_impl/DepartmentDaoJDBC.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare;
+
 import db.Db;
 import db.DbException;
 import model_dao.DepartmentDao;
@@ -55,8 +57,23 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void update(Department dp) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        PreparedStatement st = null;
+
+        try {
+            st = conn.prepareStatement(
+                "UPDATE department " +
+                "SET Name = ? " +
+                "WHERE Id = ?"
+            );
+
+            st.setString(1, dp.getName());
+            st.setInt(2, dp.getId());
+            
+            st.executeUpdate();
+        }
+        catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
@@ -67,8 +84,28 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                "SELECT department.* " +
+                "FROM department " +
+                "WHERE Id = ?" 
+            );
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if(rs.next()) {
+                Department dp = instantiateDepartment(rs);
+
+                return dp;
+            }
+            return null;
+        }
+        catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        }
     }
 
     @Override
@@ -77,4 +114,13 @@ public class DepartmentDaoJDBC implements DepartmentDao {
         throw new UnsupportedOperationException("Unimplemented method 'findAll'");
     }
     
+    //Método auxilair
+    private Department instantiateDepartment(ResultSet rs) throws SQLException{
+        Department dp = new Department();
+        dp.setId(rs.getInt("Id"));
+        dp.setName(rs.getString("Name"));
+
+        return dp;
+    }
 }
+
